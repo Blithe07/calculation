99860359e26163a34faecef0056befbe1447e9b3
laitongbin <laitongbin@yhfin.com>
2022-07-29 13:54:23 +0800

    递归-字符全排列

--- recursive.js
+++ recursive.js
@@ -41,3 +41,43 @@ function process(arr, i) {
     // 恢复数据
     arr[i] = tmp
 }
+
+/** 打印字符串全部排列，要求不重复 */
+// 思路：第一个位置N种可能性，第二种N-1种可能性...
+function Permutation(str) {
+    // 存放排列结果
+    const result = new Array()
+    if (!str || !str.length) return result
+    const arr = str.split('')
+    process1(arr, 0, result)
+    return result
+}
+// arr[i~arr.length-1]范围，i往后所有字符都可以在i位置上尝试
+// arr[0~i-1]范围，是已经做了选择的范围
+function process1(arr, i, result) {
+    // base case 当索引值超出数组长度，代表完成了一种全排列
+    if (i === arr.length) {
+        result.push(arr.toString())
+        return
+    }
+    const visit = new Array(26)
+    // i往后，做不同字符尝试
+    for (let j = i; j < arr.length; j++) {
+        // 保证不重复
+        if (!visit[arr[j] - 'a']) {
+            visit[arr[j] - 'a'] = true
+            // 交换i和j，实现多种排列情况
+            swap(arr, i, j)
+            // arr[i+1~arr.length-1]继续做选择排列
+            process1(arr, i + 1, result)
+            // 利用递归栈保存信息的特性，将数据恢复回去
+            swap(arr, i, j)
+        }
+    }
+}
+function swap(arr, i, j) {
+    const tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+}
+
